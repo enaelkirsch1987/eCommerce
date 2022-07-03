600c9689999112425e59ee68ba43c05370d86e7b
enaelkirsch1987 <107771488+enaelkirsch1987@users.noreply.github.com>
2022-07-03 18:31:52 -0300

    Criamos a classe Page

--- recursos/101 - Classe Page para o Template/index.php
+++ recursos/101 - Classe Page para o Template/index.php
@@ -1,22 +1,19 @@
 <?php 
-//
+
 require_once("vendor/autoload.php");
-//
-use Slim\Slim;
-use Hcode\Page;
-//
-$app = new Slim();
-//
+
+$app = new \Slim\Slim();
+
 $app->config('debug', true);
-//
+
 $app->get('/', function() {
-	//
-	$page = new Page();
-	//
+    
+	$page = new Hcode\Page();
+
 	$page->setTpl("index");
-	//
+
 });
-//
+
 $app->run();
-//
-?>
+
+ ?>
